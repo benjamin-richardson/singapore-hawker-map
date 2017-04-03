665468acac7c5946364b1404d08991f540c5356f
Benjamin Richardson <ben.richardson@adelphi.digital>
2017-04-03 23:56:57 +0800

    Set up first click events to highlight specific stalls that match the criteria of the cuisine selected

--- src/_modules/map/map.js
+++ src/_modules/map/map.js
@@ -1,7 +1,12 @@
 'use strict';
 
+import $ from 'jquery';
+import pad from 'pad';
+
 export default class Map {
   constructor() {
+    let _self = this;
+
     const SVG = '#asfc';
 
     this.hawkerMap = Snap.select(SVG);
@@ -28,10 +33,56 @@ export default class Map {
       , eventsListenerElement: null
     });
 
+    console.log(this.hawkerMap.selectAll('[id^=stall]')); //.animate({"fill": "#ff00ff"}, 400);
+
+    $('#cuisine-list').on('click', '.js-cuisine', function(){
+      let stallIds = _self.getIdList($(this).parent().data('id'));
+
+      console.log(stallIds);
+
+      let stallIdSelectors = _self.getIdSelectorList(stallIds);
+
+      console.log(stallIdSelectors.toString());
+
+      _self.hawkerMap.selectAll(stallIdSelectors.toString()).animate({"fill": "#ff00ff"}, 400);
+
+    });
+
   }
 
   reset() {
+    // Select all stalls and reset their colour
+    this.hawkerMap.selectAll('[id^=stall-]').animate({"fill": "#ffffff"});
+  }
 
+  getIdList(stallIdString){
+    console.log(stallIdString);
+    let stallIdArray = stallIdString.split(',');
+
+    stallIdArray = stallIdArray.map(function(dataItem, index){
+      let returnString;
+
+      if(dataItem.length === 2) {
+        returnString = pad(2, dataItem.substring(0,1), '0');
+        returnString += pad(2, dataItem.substring(1,2), '0');
+      } else if(dataItem.length === 3) {
+        returnString = pad(4, dataItem, '0');
+      } else if(dataItem.length === 4) {
+        returnString = dataItem;
+      } else {
+        console.error("Error: Invalid stall ID " + dataItem);
+      }
+
+      return returnString;
+    });
+
+    return stallIdArray;
+  }
+
+  getIdSelectorList(stallIdArray) {
+    return stallIdArray.map(function(dataItem, index){
+      return '#stall-' + dataItem + ' rect';
+    });
   }
 
   smoothZoom() {
@@ -40,6 +91,10 @@ export default class Map {
 
   smoothPan() {
 
+  }
+
+  showStalls(stallsArray) {
+    stallsArray.animate({"fill": "#ff00ff"});
   }
 
   showLayer() {
